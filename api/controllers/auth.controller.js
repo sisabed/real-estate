0303193ed1e372e09d384133d128f27bc838598c
Debasis Sahoo <121CS0221@nitrkl.ac.in>
2024-07-18 18:03:29 +0530

    sign-up page completion

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import { errorHandler } from "./error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10); //salt
-  const newUser = new User({ username, email, password });
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save(); //it takes time to create new user so "Await" and async function
     res.status(201).json("User created successfully!");
