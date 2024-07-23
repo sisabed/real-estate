17f9da550ce7e9d3fcde14478ba6c917e226dbe7
Debasis Sahoo <121CS0221@nitrkl.ac.in>
2024-07-24 02:26:50 +0530

    Completed_1

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -35,7 +35,7 @@ export default function Home() {
 
     const fetchSellListings = async () => {
       try {
-        const res = await fetch("/api/listing/get?type=sale&limit=4");
+        const res = await fetch("/api/listing/get?type=sell&limit=4");
         const data = await res.json();
         setSellListings(data);
       } catch (error) {
@@ -85,7 +85,7 @@ export default function Home() {
           ))}
       </Swiper>
 
-      {/* listing results for offer, sale and rent */}
+      {/* listing results for offer, sell and rent */}
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
         {offerListings && offerListings.length > 0 && (
           <div className="">
@@ -131,13 +131,13 @@ export default function Home() {
           <div className="">
             <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
-                Recent places for sale
+                Recent places for sell
               </h2>
               <Link
                 className="text-sm text-blue-800 hover:underline"
-                to={"/search?type=sale"}
+                to={"/search?type=sell"}
               >
-                Show more places for sale
+                Show more places for sell
               </Link>
             </div>
             <div className="flex flex-wrap gap-4">
