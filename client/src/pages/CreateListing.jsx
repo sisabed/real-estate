e6fd273051e8118b7e10098dc276933f3d348e37
Debasis Sahoo <121CS0221@nitrkl.ac.in>
2024-07-23 21:51:26 +0530

    Completed listing Page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -288,7 +288,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
-                <span className="text-xs">(Rs. / month)</span>
+                {formData.type === "rent" && (
+                  <span className="text-xs">(Rs. / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -305,7 +307,9 @@ export default function CreateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
-                  <span className="text-xs">(Rs. / month)</span>
+                  {formData.type === "rent" && (
+                    <span className="text-xs">(Rs. / month)</span>
+                  )}
                 </div>
               </div>
             )}
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -303,7 +303,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
-                <span className="text-xs">(Rs. / month)</span>
+                {formData.type === "rent" && (
+                  <span className="text-xs">(Rs. / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -320,7 +322,9 @@ export default function CreateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
-                  <span className="text-xs">(Rs. / month)</span>
+                  {formData.type === "rent" && (
+                    <span className="text-xs">(Rs. / month)</span>
+                  )}
                 </div>
               </div>
             )}
